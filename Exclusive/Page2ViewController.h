273fe424082c39a0ccf089a4cc381e95d3438a8c
Thomas Hocking <thomashocking7@gmail.com>
2015-02-17 16:16:29 -0600

    Created Xib files
    Created Xib files and the UIViews inside of them.

---
+++ Exclusive/Page2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Page2ViewController.h
+//  Exclusive
+//
+//  Created by Thomas Hocking on 2/17/15.
+//  Copyright (c) 2015 Ruben Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Page2ViewController : UIViewController
+
+@end
---
+++ Exclusive/Page3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Page3ViewController.h
+//  Exclusive
+//
+//  Created by Thomas Hocking on 2/17/15.
+//  Copyright (c) 2015 Ruben Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Page3ViewController : UIViewController
+
+@end
